e22cd1d9a0f4c85caa98ece6c43bf4ddf7c327c1
woojin065 <123820750+woojin065@users.noreply.github.com>
2024-09-18 11:35:44 +0900

    첫세팅

---
+++ src/main/java/com/example/jwj_project/JwjProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwj_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwjProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwjProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jwj_project/JwjProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwj_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwjProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
